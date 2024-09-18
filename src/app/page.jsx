9f1de9d2aff66f7289ebe2d69554e9e701f8376a
Cyanider <amihasan420@gmail.com>
2024-09-18 17:29:47 +0600

    day 1

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+export default function Home() {
+  return (
+    <div>
+      <Navbar className=""/>
+      <div className="h-12 mt-9 ">
+        Add your To-Do's here:
+        
+        
+        
+      </div>
+      <div className="container">
+          <h2>Your To-Do's</h2>
+
+      </div>
+    </div>
+  );
+}
